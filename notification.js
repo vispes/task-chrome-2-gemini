fe97f3161276484c76c880be4f40bfdb32e481b2
vispes <januacaelivirgo@gmail.com>
2025-07-01 23:22:45 +0200

    Add notification.js

---
+++ notification.js
@@ -0,0 +1,12 @@
+import { displayToast, displayModal, hideToast, hideModal } from './usernotificationsystem.js';
+
+// The functionalities previously implemented in this module have been
+// consolidated into `usernotificationsystem.js` as per architectural recommendations.
+// This module now serves as a re-export to maintain API compatibility during transition.
+
+export {
+    displayToast,
+    displayModal,
+    hideToast,
+    hideModal
+};
